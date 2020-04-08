3c30a5fe306a4f3057c88538c0f55ab9c9c9fa8c
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-04-08 11:49:37 -0400

    added opaque

--- src/Poem.js
+++ src/Poem.js
@@ -19,12 +19,30 @@ class Poem extends React.Component {
     let {title, content, author} = this.props.poem
     return (
       <div>
+        {this.state.clicked ?
+        
+        
+        <>
+        <h3 style={{opacity:.54}}>{title} </h3>
+        <p style={{opacity:.54}}>{content}</p>
+        <p>
+          <strong style={{opacity:.54}}>- By {author}</strong>
+        </p>
+        </>
+        
+        :
+       
+        <>
         <h3>{title} </h3>
         <p>{content}</p>
         <p>
           <strong>- By {author}</strong>
         </p>
-        <button onClick={this.handleClick} > {this.state.clicked ? "Marked as Read" : "Marked as Unread"}</button>
+        </>
+        }
+        
+      
+        <button onClick={this.handleClick} > {this.state.clicked ? "Mark as Read" : "Mark as Unread"}</button>
       </div>
     );
   }
