13ec1aa527e782362f4b4ca35307853ad84f8b1e
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-04-08 11:23:08 -0400

    Core Complete

--- src/Poem.js
+++ src/Poem.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 class Poem extends React.Component {
+
+  state ={
+    clicked: false
+  }
+
+  handleClick = (e) => {
+    this.setState ({
+      clicked: !this.state.clicked 
+    })
+    
+  }
+  
+
   render() {
+    // console.log(this.props.poem)
+    let {title, content, author} = this.props.poem
     return (
       <div>
-        <h3>Title</h3>
-        <p>Content</p>
+        <h3>{title} </h3>
+        <p>{content}</p>
         <p>
-          <strong>- By Author</strong>
+          <strong>- By {author}</strong>
         </p>
-        <button>Mark as read</button>
+        <button onClick={this.handleClick} > {this.state.clicked ? "Marked as Read" : "Marked as Unread"}</button>
       </div>
     );
   }
--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,11 +2,19 @@ import React from "react";
 import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
+  
+
+  arrayofComponents = () => this.props.poems.map((poem) => {
+    return <Poem key={poem.id} poem={poem}/>
+  })
+
   render() {
+    // console.log(this.props.poems)
+    // console.log(this.arrayofComponents)
     return (
       <div className="poems-container">
         {
-          // render poems here
+          this.arrayofComponents()
         }
       </div>
     );
